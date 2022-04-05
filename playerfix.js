83dd28841e531ad9cc91548aafe743b039329991
KarlKasten <jeancarlkremser@gmail.com>
2022-04-05 18:07:35 +0200

    the one and only commit

---
+++ playerfix.js
@@ -0,0 +1,16 @@
+var debug = false;
+
+var jumper = document.getElementsByClassName("jumper");
+if(debug) console.log(jumper);
+jumper[0].style.height = "90.5%";
+jumper[1].style.height = "90.5%";
+
+var player = document.getElementById("mediaplayer");
+if(debug) console.log(player);
+player.controls = true;
+
+var fullscreen = document.getElementsByClassName("no_fullscreen");
+if(debug) console.log(fullscreen);
+fullscreen1[0].remove();
+
+if(debug) console.log("Done");
